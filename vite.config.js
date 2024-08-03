e64236102972d24bae135d74f400975c3adcdb4b
Rahul Pareek <41017269+bisomaticc@users.noreply.github.com>
2024-08-03 13:02:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@mui/material', '@emotion/react', '@emotion/styled']
+    include: ['@mui/material', '@emotion/react', '@emotion/styled','axios']
   },
   build: {
     rollupOptions: {
